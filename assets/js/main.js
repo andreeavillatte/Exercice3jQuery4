5f9ce0c9672ae2cefc3ee720bd3a2f8f32ac956a
core.editor <andreeavillatte@gmail.com>
2019-04-30 13:52:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// Quand le document est ready
+$(function () {
+    // Faire de la validation de formulaire ///////////////////////////::
+    // lorsque le formulaire va etre soumis
+    $('form').submit(function (event) {
+        //on recupere toutes las inputs; je les mets dans une seule variable
+        // on va creer comme "query selecteur all"
+        var inputs = $('input');
+        
+        $('small').remove();
+        // on va parcourir ce tableau pour traiter chaque colonne du tableau
+        // cette methode (.each) permet de parcourir mon tableau
+        $.each(inputs, function () {
+            let input = this;
+            // on verifie que nos champs respectent les contraintes ( requires, maxlength, etc.) qu'on a defini dans notre HTML
+            if (!input.validity.valid) {
+                var small = $('<small></small>');
+                small.addClass('alert alert-danger');
+                small.text(input.validationMessage);
+                $(input).after(small);
+            };
+            event.preventDefault();
+        });
+    });
+})
---
+++ assets/js/main2.js
@@ -0,0 +1,72 @@
+$(function () {
+    var regexName = /^[A-Za-zéÉàÀîÎçÇëËïêÊèÈ]+([-]?[A-Za-zéÉàÀîÎçÇëËïêÊèÈ])*$/;
+    var regexPhone = /^0[67][0-9]{8}$/;
+    var regexMail = /^[a-zA-Z][a-zA-Z0-9]+([_.-]?[a-zA-Z0-9]+)@[a-zA-Z]+.[a-zA-Z]{2,5}$/;
+
+    $('form').submit(function (event) {
+        var name = $('#Name').val();
+        var firstname = $('#firstName').val();
+        var mail = $('#Email').val();
+        var phone = $('#number').val();
+        $('small').remove();
+        //////////////// NAME //////////////////////////////////////////////////////
+        if (name == '') {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Ce champs est obligatoire');
+            $('#Name').after(small);
+        }
+        else if (!regexName.test(name)) {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Nom Invalide !');
+            $('#Name').after(small);
+        }
+        event.preventDefault();
+        ////////////////// FIRST NAME //////////////////////////////////////////////
+        if (firstname == '') {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Ce champs est obligatoire');
+            $('#firstName').after(small);
+        }
+        else if (!regexName.test(first)) {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Prenom Invalide !');
+            $('#firstName').after(small);
+        }
+        event.preventDefault();
+
+        ////////////////// EMAIL //////////////////////////////////////////////
+        if (mail == '') {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Ce champs est obligatoire');
+            $('#Email').after(small);
+        }
+        else if (!regexMail.test(mail)) {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Email invalide !')  ;
+            $('#Email').after(small);
+        }
+        event.preventDefault();
+
+        ////////////////// PHONE //////////////////////////////////////////////
+        if (phone == '') {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Ce champs est obligatoire');
+            $('#number').after(small);
+        }
+        else if (!regexPhone.test(phone)) {
+            var small = $('<small></small>');
+            small.addClass('alert alert-danger');
+            small.text('Numéro de téléphone invalide !');
+            $('#number').after(small);
+        }
+        event.preventDefault();
+
+    })
+    });
